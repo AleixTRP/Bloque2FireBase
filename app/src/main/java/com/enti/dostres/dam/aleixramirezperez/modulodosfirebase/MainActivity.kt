5969c95e497d767826d33eaef507261721334efc
AzureAD\ALEIXRAMIREZIPEREZ <aleix.ramirez@enti.cat>
2023-11-06 16:30:02 +0100

    Firebase

--- app/src/main/java/com/enti/dostres/dam/aleixramirezperez/modulodosfirebase/MainActivity.kt
+++ app/src/main/java/com/enti/dostres/dam/aleixramirezperez/modulodosfirebase/MainActivity.kt
@@ -1,4 +1,13 @@
 package com.enti.dostres.dam.aleixramirezperez.modulodosfirebase
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity {
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?,) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
 }
